c6439c716701a6345e4668ee15ae392a7f2f7f5a
QuantumSheep <quantumsheep@outlook.fr>
2018-05-22 01:15:39 +0200

    Add modals library

---
+++ assets/lib/modalbird/modalbird.js
@@ -0,0 +1,63 @@
+class ModalBird {
+    static init() {
+        document.querySelectorAll('.modalbird[data-modal]').forEach(modal => {
+            modal.style.paddingRight = this.getScrollbarWidth() + 'px';
+
+            modal.querySelectorAll('[data-action=close]').forEach(closebtn => {
+                closebtn.addEventListener('click', e => {
+                    e.preventDefault();
+                    this.hide(modal.getAttribute('data-modal'));
+                });
+            });
+
+            modal.addEventListener('click', e => {
+                if (e.target == modal) {
+                    this.hide(modal.getAttribute('data-modal'));
+                }
+            });
+        });
+    }
+
+    static hide(modal) {
+        document.querySelectorAll(`.modalbird[data-modal=${modal}]`).forEach(modal => {
+            modal.classList.add('hide');
+
+            setTimeout(() => {
+                document.body.style.paddingRight = null;
+                document.body.classList.remove('modalbird-isopen');
+            }, 300);
+        });
+    }
+
+    static show(modal) {
+        document.body.classList.add('modalbird-isopen');
+        document.body.style.paddingRight = this.getScrollbarWidth() + 'px';
+
+        document.querySelectorAll(`.modalbird[data-modal=${modal}]`).forEach(modal => {
+            modal.scrollTop = 0;
+            modal.classList.remove('hide');
+        });
+    }
+
+    static getScrollbarWidth() {
+        const outer = document.createElement("div");
+        outer.style.visibility = "hidden";
+        outer.style.width = "100px";
+        outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+
+        document.body.appendChild(outer);
+
+        const widthNoScroll = outer.offsetWidth;
+        outer.style.overflow = "scroll";
+
+        const inner = document.createElement("div");
+        inner.style.width = "100%";
+        outer.appendChild(inner);
+
+        const widthWithScroll = inner.offsetWidth;
+
+        outer.parentNode.removeChild(outer);
+
+        return widthNoScroll - widthWithScroll;
+    }
+}
